822ffa9ff2d1c9cd1fc591635634964254981fdf
ykkim <44081957+ykkim-git@users.noreply.github.com>
2021-01-13 21:53:28 +0900

    subscribe and theory of redux

--- README.md
+++ README.md
@@ -21,7 +21,8 @@ console.log(store.getState()) // print 0
 ___
 
 ## Actions
-
+1. dispatch call reducer
+2. reducer modifying data of store
 ```js
 
 const add = document.querySelector("#add"),
@@ -49,3 +50,52 @@ countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState()); // print 2(3 - 1);
 
 ```
+
+___
+
+## Subscribe: store안에서 일어나는 변화를 감지한다.
+```js
+const add = document.querySelector("#add"),
+  minus = document.querySelector("#minus"),
+  number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+/** subscribe: store안에서 일어나는 변화를 감지한다. */
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+// dispatch argument should be object
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+```
+
+
--- src/index.js
+++ src/index.js
@@ -4,22 +4,39 @@ const add = document.querySelector("#add"),
   minus = document.querySelector("#minus"),
   number = document.querySelector("span");
 
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action)
-  if (action.type === "ADD") {
-    return (count += 1);
-  } else if (action.type === "MINUS") {
-    return (count -= 1);
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" }); // redux call modifier
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+/** subscribe: store안에서 일어나는 변화를 감지한다. */
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState()); // print 2(3 - 1);
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+// dispatch argument should be object
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
